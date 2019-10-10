0d8c32d1ddc5961e74f1d4d597d9244281c43a88
Paul Gorman <paul.gorman@oath.com>
2019-10-10 09:05:54 +0100

    new Java exercise no. 34

---
+++ src/main/java/JavaExercises/JavaExercise34.java
@@ -0,0 +1,13 @@
+package JavaExercises;
+
+public class JavaExercise34 {
+}
+
+//34. Write a Java program to compute the area of a hexagon. Go to the editor
+//        Area of a hexagon = (6 * s^2)/(4*tan(π/6))
+//        where s is the length of a side
+//        Input Data:
+//        Input the length of a side of the hexagon: 6
+//        Expected Output
+//
+//        The area of the hexagon is: 93.53074360871938
