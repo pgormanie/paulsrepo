c82b9c0175f0b39d607737a01d5bd7439e2a1215
Paul Gorman <paul.gorman@oath.com>
2019-10-17 09:10:53 +0100

    Added another Java exercise no. 36

---
+++ src/main/java/JavaExercises/JavaExercise36.java
@@ -0,0 +1,17 @@
+package JavaExercises;
+
+public class JavaExercise36 {
+}
+
+//36. Write a Java program to compute the distance between two points on the surface of earth. Go to the editor
+//        Distance between the two points [ (x1,y1) & (x2,y2)]
+//        d = radius * arccos(sin(x1) * sin(x2) + cos(x1) * cos(x2) * cos(y1 - y2))
+//        Radius of the earth r = 6371.01 Kilometers
+//        Input Data:
+//        Input the latitude of coordinate 1: 25
+//        Input the longitude of coordinate 1: 35
+//        Input the latitude of coordinate 2: 35.5
+//        Input the longitude of coordinate 2: 25.5
+//        Expected Output
+//
+//        The distance between those points is: 1480.0848451069087 km
